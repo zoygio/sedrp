c6373989fe0833b2a9852c9315e9ccb0e4b780fa
zoygio <115259097+zoygio@users.noreply.github.com>
2024-05-23 00:00:30 -0600

    Add files via upload

---
+++ sedrp/sedrp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sedrp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
